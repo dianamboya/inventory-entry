bba765d09930a06079cef40c96d95961e79bb656
ritawanjiru27-png <ritawanjiru27@gmail.com>
2026-03-16 12:23:17 +0300

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@
 export const BLOCKS = {
   "Block A": { floors: ["2", "4", "5", "6", "7", "8", "14", "22"] },
   "Block B": { floors: ["14", "15"] },
-  "Block C": { floors: ["23", "5", "6", "7"] },
+  "Block C": { floors: ["2", "3", "5", "6", "7"] },
 
   // ✅ New option: DOSH has NO floors
   "DOSH": { floors: [] }
